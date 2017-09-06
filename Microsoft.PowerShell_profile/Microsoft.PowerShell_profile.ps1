ca215bcbeecec0389fff06bd4d38c7056fd4ea76
Joe Scalise <joe@scalise.us>
2017-09-06 11:17:36 -0400

    Copy latest version of itself from local repository

--- Microsoft.PowerShell_profile/Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile/Microsoft.PowerShell_profile.ps1
@@ -44,6 +44,7 @@ Set-Alias open Invoke-Item
 # Simple shortcut for creating files, like on Linux
 Set-Alias touch New-Item
 
+# Show date and time
 Get-Date
 
 if (!(Test-Path "C:\temp\secrets\scriptpath.sec")) {
@@ -51,6 +52,17 @@ if (!(Test-Path "C:\temp\secrets\scriptpath.sec")) {
   return
 } else {
   $scriptPath = Get-Content "C:\temp\secrets\scriptpath.sec"
+}
+
+# Keep updated copy of PowerShell profile from Github local repository
+$profileDirectory = "$env:USERPROFILE\Documents\WindowsPowerShell\"
+$profileRepository = "$scriptPath\Microsoft.PowerShell_profile"
+if ((Get-ChildItem "$profileDirectory\Microsoft.PowerShell_profile.ps1").LastWriteTime -lt (Get-ChildItem "$profileRepository\Microsoft.PowerShell_profile.ps1").LastWriteTime) {
+  try { 
+    Copy-Item "$profileRepository\Microsoft.PowerShell_profile.ps1" -Destination $profileDirectory -Force 
+    Restart-Profile
+  }
+  catch { Write-Warning "Error copying updated profile from local repository." }
 }
 
 function Get-Weather {
@@ -63,4 +75,5 @@ function Get-Weather {
   if (!$NoSpace) { Write-Host }
 }
 
+# Show the weather forecast
 Get-Weather -NoSpace
